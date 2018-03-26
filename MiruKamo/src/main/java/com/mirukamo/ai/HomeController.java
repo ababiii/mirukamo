6b561ab40e3665520bdf61b5cbf1feaa6d7cd221
uemjoenghwan <uem2530@gmail.com>
2018-03-26 10:15:55 +0900

    주석주석주석

--- MiruKamo/src/main/java/com/mirukamo/ai/HomeController.java
+++ MiruKamo/src/main/java/com/mirukamo/ai/HomeController.java
@@ -32,7 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//엄정환
+		//엄정환 주석
+		  
 		return "home";
 	}
 	

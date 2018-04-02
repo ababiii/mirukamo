0bbf84f466f5d67a147da635c3a67b4fbabef2c0
uemjoenghwan <uem2530@gmail.com>
2018-04-02 11:03:19 +0900

    1

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.text.DateFormat;
 
 import java.util.Date;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 
 /**
--- src/dao/UsersDAO.java
+++ src/dao/UsersDAO.java
@@ -1,10 +1,8 @@
 package dao;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-import com.project_practice.www.VO.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Repository
 public class UsersDAO {
